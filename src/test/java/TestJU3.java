a4803c8d697f39c02554eeb567aaf9fb5f8b8190
NadezhdaBul <Bulygina_n_a@mail.ru>
2023-03-17 00:27:14 +0300

    HW-1

--- src/test/java/TestJU3.java
+++ src/test/java/TestJU3.java
@@ -1,10 +1,14 @@
 import com.codeborne.selenide.Configuration;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
@@ -24,15 +28,19 @@ public class TestJU3 {
 
     @Tags({@Tag("Critical"), @Tag("FindTV")})
 
-    @CsvSource ({
-            "43NANO776QA, LG 43NANO776QA",
-            "32DB2E, Sharp 32DB2E",
-            "50NANO769QA, LG 50NANO769QA"
-    })
+    static @NotNull Stream <Arguments> testFindTV (){
+        return Stream.of(
+                Arguments.of("43NANO776QA", "LG 43NANO776QA"),
+                Arguments.of("32DB2E", "Sharp 32DB2E"),
+                Arguments.of("50NANO769QA", "LG 50NANO769QA")
+        );
+    }
+
+    @MethodSource("testFindTV")
 
     @ParameterizedTest (name="в результате поиска модели {0} дожен отобразиться товар с наименованием {1}")
 
-    void testFindSite (
+    void testFindModel(
         String findText,
         String foundModel
     )
