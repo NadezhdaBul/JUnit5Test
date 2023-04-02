d4c45428d7b059b37f8a811b0856fe08f475ecf0
NadezhdaBul <Bulygina_n_a@mail.ru>
2023-04-02 22:19:00 +0300

    Telegram-report

--- src/test/java/TestJU2.java
+++ src/test/java/TestJU2.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-public class TestJU2 extends TestBase {
+public class TestJU2 {
 
 
     @BeforeEach
